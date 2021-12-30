b7e8be00865dfa19aea20a4628bff7f0fb187853
rumikoacopa <rumikorios@gmail.com>
2021-12-30 12:32:58 -0500

    rm javascript pack tag code in appl.html.erb, fixed bug however need better solution

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start();
 require("channels");
 require("jquery");
 
-import '../stylesheets/style';
+import '../stylesheets/application'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
