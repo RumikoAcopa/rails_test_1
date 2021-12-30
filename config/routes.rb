d084717fa2d7772287f05c3d034581ceafc86e9c
rumikoacopa <rumikorios@gmail.com>
2021-12-29 19:11:36 -0500

    practicing changing root route to go to a controller I specify-in this case applications controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'application'
 end
+
+#want this root route to go to a controller. In this case our application controller
