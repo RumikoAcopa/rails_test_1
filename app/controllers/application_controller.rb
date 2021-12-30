48e61a33124ba1f2dafb8dc552d561034d67f82e
rumikoacopa <rumikorios@gmail.com>
2021-12-29 19:52:28 -0500

    removed hello method and changed root route to pages w/home action to send request too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
 
-  def hello
-    render html: 'Hello World!'
-    #generates html template with hello world. (render == 'display')
-  end
-
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'application'
+  root 'pages#home'
 end
 
 #want this root route to go to a controller. In this case our application controller
